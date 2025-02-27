f32b7490d92cf61127498a3083b5f300396e071f
sukhovaul <sukhovaul@yandex.ru>
2025-02-27 19:40:15 +0300

    27.02.2025

---
+++ resource_manager.py
@@ -0,0 +1,10 @@
+import pygame as pg
+import pytmx
+
+class ResourceManager:
+    def __init__(self):
+        self.maps = {}
+
+    def load_map(self, map_name, file_path):
+        self.maps[map_name] = pytmx.load_pygame(file_path)
+        return self.maps[map_name]
---
+++ settings.py
@@ -0,0 +1,7 @@
+# Базовые настройки
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 600
+TITLE = "Plants vs Zombies"
+FPS = 60
+STARTING_SUN = 100
+LEVEL1_MAP = 'maps/level1.tmx'
