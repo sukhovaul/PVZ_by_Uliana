5b07ce9fad0d08c0fc31fa975a1c1cdad7748013
sukhovaul <sukhovaul@yandex.ru>
2025-03-27 19:17:44 +0300

    27.03.2025

--- cells.py
+++ cells.py
@@ -17,6 +17,8 @@ class Cells():
         print(self.cells)
 
         self.sunflower_picture = pg.image.load('pictures/plants/SunFlower.png')
+        self.wallnut_picture = pg.image.load('pictures/plants/WallNut_0.png')
+        self.peashooter_picture = pg.image.load('pictures/plants/Peashooter_0.png')
     def fill_cell(self, pos, plant, plant_amount, sun_object):
         for cell in self.cells:
             if cell["rect"].collidepoint(pos) and plant.active_plant:
@@ -25,9 +27,14 @@ class Cells():
                     if sun_object.suns_total >= plant_amount:
                         cell["empty"] = False
                         sun_object.suns_total -= plant_amount
-                        print(sun_object.suns_total)
                         if plant.active_plant == 'sunflower':
                             self.plants.append({"image":self.sunflower_picture,"x":cell["rect"].x, "y":cell["rect"].y})
+                            plant.active_plant = None
+                        elif plant.active_plant == 'wallnut':
+                            self.plants.append({"image": self.wallnut_picture, "x":cell["rect"].x, "y":cell["rect"].y})
+                            plant.active_plant = None
+                        elif plant.active_plant:
+                            self.plants.append({"image": self.peashooter_picture, "x": cell["rect"].x, "y": cell["rect"].y})
                             plant.active_plant = None
                     else:
                         print('Недостаточно солнц для покупки растения')
---
+++ zombies.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+class Zombies():
+    def __init__(self):
+        self.zombie_image_0 = pg.image.load('pictures/zombies/Zombie_0.png')
